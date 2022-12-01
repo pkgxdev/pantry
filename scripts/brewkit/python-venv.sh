abe2f46ecd2c2519bab4dc9846c086071b369868
James Reynolds <magnsuviri@me.com>
2022-12-01 11:30:51 -0700

    Write shim file

--- scripts/brewkit/python-venv.sh
+++ scripts/brewkit/python-venv.sh
@@ -18,7 +18,19 @@ ln -s ../libexec/bin/$CMD_NAME $CMD_NAME
 cd ../libexec/bin
 fix-shebangs.ts *
 
-cp "$SRCROOT"/props/shim.bash $CMD_NAME
+cat <<EOF>$CMD_NAME
+#!/usr/bin/env bash
+self="\${BASH_SOURCE[0]}"
+if test -L "\$self"; then
+  prefix="\$(dirname "\$self")"
+  suffix="\$(dirname \$(readlink \$self))"
+  LIBEXEC="\$(cd \$prefix/\$suffix && pwd)"
+else
+  LIBEXEC="\$(cd "\$(dirname "\$self")" && pwd)"
+fi
+source "\$LIBEXEC/activate"
+exec "\$LIBEXEC"/$CMD_NAME.py "\$@"
+EOF
 chmod +x $CMD_NAME
 
 rm Activate.ps1 activate.csh activate.fish
