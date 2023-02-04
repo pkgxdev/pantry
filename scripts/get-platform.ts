63632639a0fb25caaed83d57a6f0800f9e124469
Jacob Heider <jacob@tea.xyz>
2023-02-04 18:49:21 -0500

    use actions/cache to reduce deno.land 500s

--- scripts/get-platform.ts
+++ scripts/get-platform.ts
@@ -11,56 +11,84 @@ args:
 
 import { panic } from "utils";
 
-const platform = Deno.env.get("PLATFORM") ?? panic("$PLATFORM not set")
-
-let os: string | string[]
-let buildOs: string | string[]
-let container: string | undefined
-let testMatrix: { os: string | string[], container: string | undefined }[]
-
-switch(platform) {
-  case "darwin+x86-64":
-    os = "macos-11"
-    // Using GHA resources for now, until we resolve network issues with our runners
-    // buildOs = ["self-hosted", "macOS", "X64"]
-    buildOs = os
-    testMatrix = [{ os, container: undefined }]
-    break
-  case "darwin+aarch64":
-    os = ["self-hosted", "macOS", "ARM64"]
-    buildOs = os
-    testMatrix = [{ os, container: undefined }]
-    break
-  case "linux+aarch64":
-    os = ["self-hosted", "linux", "ARM64"]
-    buildOs = os
-    testMatrix = [{ os, container: undefined }]
-    break
-  case "linux+x86-64":
-    os = "ubuntu-latest"
-    // Using GHA resources for now, until we resolve network issues with our runners
-    // buildOs = ["self-hosted", "linux", "X64"]
-    buildOs = os
-    container = "ghcr.io/teaxyz/infuser:latest"
-    testMatrix = [
-      { os, container: undefined },
-      // { os: buildOs, container: undefined },
-      { os, container },
-      { os, container: "debian:buster-slim" },
-    ]
-    break
-  default:
-    panic(`Invalid platform description: ${platform}`)
+type Output = {
+  os: OS,
+  buildOs: OS,
+  container?: string,
+  testMatrix: { os: OS, container?: string }[]
+  cacheSet: string
 }
 
-const output = `os=${JSON.stringify(os)}\n` +
-  `build-os=${JSON.stringify(buildOs)}\n` +
-  `container=${JSON.stringify(container)}\n` +
-  `test-matrix=${JSON.stringify(testMatrix)}\n`
+type OS = string | string[]
 
-Deno.stdout.write(new TextEncoder().encode(output))
+const platform = Deno.env.get("PLATFORM") ?? panic("$PLATFORM not set")
+
+const cacheSets = {
+  "darwin": "$HOME/Library/Caches/deno/deps/https/",
+  "linux": "$HOME/.cache/deno/deps/https/"
+}
+
+const output: Output = (() => {
+  switch(platform) {
+  case "darwin+x86-64": {
+    // Using GHA resources for now, until we resolve network issues with our runners
+    // buildOs: ["self-hosted", "macOS", "X64"]
+    const os = "macos-11"
+    return {
+      os,
+      buildOs: os,
+      testMatrix: [{ os }],
+      cacheSet: cacheSets["darwin"]
+    }
+  }
+  case "darwin+aarch64": {
+    const os = ["self-hosted", "macOS", "ARM64"]
+    return {
+      os,
+      buildOs: os,
+      testMatrix: [{ os }],
+      cacheSet: cacheSets["darwin"]
+    }
+  }
+  case "linux+aarch64": {
+    const os = ["self-hosted", "linux", "ARM64"]
+    return {
+      os,
+      buildOs: os,
+      testMatrix: [{ os }],
+      cacheSet: cacheSets["linux"]
+    }
+  }
+  case "linux+x86-64": {
+    // Using GHA resources for now, until we resolve network issues with our runners
+    // buildOs: ["self-hosted", "linux", "X64"]
+    // testMatrix.push({ os: buildOs, container: undefined })
+    const os = "ubuntu-latest"
+    const container = "ghcr.io/teaxyz/infuser:latest"
+    return { os,
+      buildOs: os,
+      container,
+      testMatrix: [
+        { os },
+        { os, container },
+        { os, container: "debian:buster-slim" }
+      ],
+      cacheSet: cacheSets["linux"]
+    }
+  }
+  default:
+    panic(`Invalid platform description: ${platform}`)
+}})()
+
+const rv = `os=${JSON.stringify(output.os)}\n` +
+  `build-os=${JSON.stringify(output.buildOs)}\n` +
+  `container=${JSON.stringify(output.container)}\n` +
+  `test-matrix=${JSON.stringify(output.testMatrix)}\n` +
+  `cache-set=${JSON.stringify(output.cacheSet)}\n`
+
+Deno.stdout.write(new TextEncoder().encode(rv))
 
 if (Deno.env.get("GITHUB_OUTPUT")) {
   const envFile = Deno.env.get("GITHUB_OUTPUT")!
-  await Deno.writeTextFile(envFile, output, { append: true})
+  await Deno.writeTextFile(envFile, rv, { append: true})
 }
