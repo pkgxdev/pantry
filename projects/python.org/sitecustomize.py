601352272521347c1d5df7cf99e4669337c608d2
Max Howell <mxcl@me.com>
2023-03-20 09:10:55 -0400

    fix `python -m venv` not providing pip

--- projects/python.org/sitecustomize.py
+++ projects/python.org/sitecustomize.py
@@ -1,10 +1,11 @@
 import site
 import sys
 
-# tea considers /usr/local the “πglobal install” location
-# setting this is kinda nuts but brew does the same so
-# we’re assuming they vetted all options
-sys.prefix = "/usr/local"
+# tea considers /usr/local the “global install” location
+# setting `sys.prefix` is kinda nuts but brew does the same
+# so we’re assuming they vetted all options
+# sys.prefix = "/usr/local"
+# ^^ undid this because it broke `python -m venv` installing pip for some reason
 
 # enable automatic user installs if pip makes the decision
 # that this is good idea. eg. if /usr/local isn’t writable
