3f8917551bed32c61c97ffad0e61b3c6375675e0
Sanchit Ram Arvind <sanchitram@gmail.com>
2023-04-10 14:05:39 -0400

    fix-up tbin file

--- projects/github.com/ggerganov/whisper.cpp/whisper.cpp
+++ projects/github.com/ggerganov/whisper.cpp/whisper.cpp
@@ -5,15 +5,9 @@ test -n "$VERBOSE" && set -x
 
 D="$(cd "$(dirname "$0")"/.. && pwd)"
 VERSION="$(basename "$D")"
-MODEL_DIR="$./models"
+MODEL_DIR=""$D"/tbin/models"
 
-export PATH="$D/tbin:$PATH"
-
-// llama-fetch "$MODEL_DIR" "$VERSION"
-
-echo $1 
 echo $D 
-echo $MODEL_DIR 
 
 if test $1 = chat; then
   exec "$D"/tbin/whisper.cpp \
