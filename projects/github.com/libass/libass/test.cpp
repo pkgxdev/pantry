53aa0ff0d7fa51ac9c621d5957dfd6e8a067a3d8
Joe DeCapo <joe@polka.cat>
2023-05-06 15:42:30 -0500

    Fix formatting

--- projects/github.com/libass/libass/test.cpp
+++ projects/github.com/libass/libass/test.cpp
@@ -1,21 +1,20 @@
 #include "ass/ass.h"
+
 int main() {
-ASS_Library *library;
-ASS_Renderer *renderer;
-library = ass_library_init();
-if (library) {
-    renderer = ass_renderer_init(library);
+    ASS_Library *library;
+    ASS_Renderer *renderer;
+    library = ass_library_init();
+    if (library) {
+        renderer = ass_renderer_init(library);
         if (renderer) {
-        ass_renderer_done(renderer);
-        ass_library_done(library);
-        return 0;
+            ass_renderer_done(renderer);
+            ass_library_done(library);
+            return 0;
+        } else {
+            ass_library_done(library);
+            return 1;
         }
-        else {
-        ass_library_done(library);
-        return 1;
-        }
-    }
-    else {
+    } else {
         return 1;
     }
 }
