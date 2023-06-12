cc5ec8e03ece103224cf408d4826954cd0506656
ABevier <awbevier@gmail.com>
2023-06-12 19:10:04 -0400

    publish to new packages topic

--- .github/scripts/index-packages.ts
+++ .github/scripts/index-packages.ts
@@ -3,9 +3,13 @@
 import { usePantry } from "hooks"
 import * as ARGV from "./utils/args.ts"
 import { SQSClient, SendMessageCommand } from "npm:@aws-sdk/client-sqs@^3"
+import { SNSClient, PublishCommand } from "npm:@aws-sdk/client-sns"
 import { panic } from "utils"
 
-const sqsClient = new SQSClient({ region: Deno.env.get("AWS_REGION") ?? panic("No region specified") })
+const region = Deno.env.get("AWS_REGION") ?? panic("No region specified")
+const sqsClient = new SQSClient({ region })
+const snsClient = new SNSClient({ region })
+
 const pantry = usePantry()
 
 const pkgs = await ARGV.toArray(ARGV.pkgs())
@@ -20,6 +24,8 @@ for(const pkg of pkgs) {
       github: yml?.versions?.github || "",
       // TODO: add other useable data here eventually
     }
+
+    // DEPRECATED: This is the old way of doing things
     const res = await sqsClient.send(new SendMessageCommand({
       MessageGroupId: 'project',
       MessageDeduplicationId: project,
@@ -27,6 +33,15 @@ for(const pkg of pkgs) {
       QueueUrl: Deno.env.get("SQS_GENERATE_PACKAGE_DETAILS_URL")!,
     }))
     console.log(`SQS task for pkg:${project} messageId:${res.MessageId}`)
+
+    const snsMessage = await snsClient.send(new PublishCommand({
+      TopicArn: Deno.env.get("SNS_NEW_PACKAGES_TOPIC")!,
+      Message: JSON.stringify(taskMessage),
+      MessageGroupId: 'project',
+      MessageDeduplicationId: project,
+    }))
+
+    console.log(`SNS message published for pkg:${project} messageId:${snsMessage.MessageId}`)
   } catch (error) {
     console.error(error);
   }
