62ee5f35764afaa1f2642c2ce8ce29a430bd1803
Andrii Riabchenko <ryabchenko.andrew@gmail.com>
2023-09-06 14:23:37 +0300

    wip

--- projects/luajit.org/test.lua
+++ projects/luajit.org/test.lua
@@ -1,3 +1,11 @@
-local ffi = require("ffi")
-ffi.cdef("int printf(const char *fmt, ...);")
-ffi.C.printf("Hello %s!\\n", "World")
+function fibonacci(n)
+    if n <= 0 then
+        return 0
+    elseif n == 1 then
+        return 1
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    end
+end
+
+print("Fibonacci(10) =", fibonacci(10))
