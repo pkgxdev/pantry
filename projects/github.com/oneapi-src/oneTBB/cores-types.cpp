073c6d4132d3cd2f55f731fd9be64a0ad711c3ec
Andrii Riabchenko <ryabchenko.andrew@gmail.com>
2023-10-06 11:10:36 +0300

    wip

--- projects/github.com/oneapi-src/oneTBB/cores-types.cpp
+++ projects/github.com/oneapi-src/oneTBB/cores-types.cpp
@@ -4,17 +4,10 @@
 int main() {
     const auto max_concurrency = tbb::this_task_arena::max_concurrency();
 
-#ifdef __APPLE__
     if (max_concurrency == 1) {
-        std::cerr << "Error: Invalid conditions on MacOS." << std::endl;
+        std::cerr << "Error: Invalid conditions." << std::endl;
         return EXIT_FAILURE;
     }
-#else
-    if (max_concurrency != 1) {
-        std::cerr << "Error: Invalid conditions on Linux." << std::endl;
-        return EXIT_FAILURE;
-    }
-#endif
 
     std::cout << "Conditions are valid." << std::endl;
     return EXIT_SUCCESS;
