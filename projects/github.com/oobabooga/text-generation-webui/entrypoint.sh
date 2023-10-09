fe4553718c175c234572c0eed70dabdbd3369e47
Jacob Heider <jacob@pkgx.dev>
2023-10-08 21:35:21 -0400

    fix(text-generation-webui) (#3579)
    * fix(text-generation-webui)
    
    closes #3576
    
    * let's see if this works

--- projects/github.com/oobabooga/text-generation-webui/entrypoint.sh
+++ projects/github.com/oobabooga/text-generation-webui/entrypoint.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env -S tea bash
+#!/usr/bin/env -S pkgx bash
+# shellcheck shell=bash
 
-PORT=$(tea get-port)
+PORT=$(pkgx get-port)
 
 set -emfo pipefail
 
-tea gum format <<EoMD
+pkgx gum format <<EoMD
 # loading llama.cpp model…
 
 this may take a while…
@@ -13,14 +14,14 @@ EoMD
 
 echo  # spacer
 
-tea llama.cpp --fetch
+pkgx llama.cpp --fetch
 
-d="$(cd "$(dirname $0)" && pwd)"
+d="$(cd "$(dirname "$0")" && pwd)"
 
 XDG="${XDG_DATA_HOME:-$HOME/.local/share}"
 
 "$d"/bin/text-generation-webui \
-  --listen-port $PORT \
+  --listen-port "$PORT" \
   --model-dir "$XDG/models" \
   --model OpenLLaMA \
   &
@@ -28,7 +29,7 @@ XDG="${XDG_DATA_HOME:-$HOME/.local/share}"
 PID=$!
 
 # poll until a HEAD request succeeds
-while ! curl -Is http://127.0.0.1:$PORT | grep -q "HTTP/1.1 200 OK"; do
+while ! curl -Is http://127.0.0.1:"$PORT" | grep -q "HTTP/1.1 200 OK"; do
   if ! kill -0 $PID; then
     echo "webui process died!"
     exit 1
@@ -37,20 +38,21 @@ while ! curl -Is http://127.0.0.1:$PORT | grep -q "HTTP/1.1 200 OK"; do
 done
 
 # open the URL once the HEAD request succeeds
-if test -n "$TEA_GUI"; then
-  echo "{\"xyz.tea\":{\"gui\":\"http://127.0.0.1:$PORT\"}}" >&2
+# shellcheck disable=SC2154
+if test -n "$pkgx_GUI"; then
+  echo "{\"xyz.pkgx\":{\"gui\":\"http://127.0.0.1:$PORT\"}}" >&2
 else
   open "http://127.0.0.1:$PORT"
 fi
 
-tea gum format <<EoMD
+pkgx gum format <<EoMD
 # text generation web UI
 
 this package has been modified for your convenience:
 
 * download additional models to \`$XDG/models\`
 
-> bugs reports to our [tracker](https://github.com/teaxyz/pantry/issues). thanks!
+> bugs reports to our [tracker](https://github.com/pkgxxyz/pantry/issues). thanks!
 
 enjoy!
 EoMD
