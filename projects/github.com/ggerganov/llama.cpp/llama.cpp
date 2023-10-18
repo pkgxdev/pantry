9924a8e23345cfbd1f88a1992bdb6580b835b94f
James Reynolds <magnsuviri@me.com>
2023-10-17 18:29:40 -0600

    llama.cpp, check if model is specified, if yes, run it, if not, then download model

--- projects/github.com/ggerganov/llama.cpp/llama.cpp
+++ projects/github.com/ggerganov/llama.cpp/llama.cpp
@@ -4,8 +4,19 @@ set -e
 test -n "$VERBOSE" && set -x
 
 D="$(cd "$(dirname "$0")"/.. && pwd)"
+
+if [ "$1" != '--fetch' ]; then
+  # detect --model arg or not
+  for arg in "$@"; do
+    if [ "$arg" = "--model" -o  "$arg" = "-m" ]; then
+    exec "$D"/libexec/llama.cpp "$@"
+    fi
+  done
+fi
+
 VERSION="$(basename "$D")"
 MODEL_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/models/OpenLLaMA"
+MODEL_FILE="$MODEL_DIR/OpenLLaMA-3Bv2.ggml.f16.bin"
 
 "$D"/libexec/llama-fetch "$MODEL_DIR" "$VERSION"
 
@@ -15,7 +26,7 @@ fi
 
 if [ $# -eq 0 ]; then
   exec "$D"/libexec/llama.cpp \
-    --model "$MODEL_DIR/OpenLLaMA-3Bv2.ggml.f16.bin" \
+    --model "$MODEL_FILE" \
     --ctx-size 512 \
     --batch-size 1024 \
     --n-predict 256 \
@@ -27,11 +38,4 @@ if [ $# -eq 0 ]; then
     --file "$D"/share/prompts/chat-with-bob.txt
 fi
 
-# detect --model arg or not
-for arg in "$@"; do
-  if [ "$arg" = "--model" -o  "$arg" = "-m" ]; then
-    exec "$D"/libexec/llama.cpp "$@"
-  fi
-done
-
-exec "$D"/libexec/llama.cpp "$@" --model "$MODEL_DIR/OpenLLaMA-3Bv2.ggml.f16.bin"
+exec "$D"/libexec/llama.cpp "$@" --model "$MODEL_FILE"
