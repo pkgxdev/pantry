d3ec5f0a3b779e6eae93b5d0fc0b688fd2d8a3a9
Andrii Riabchenko <ryabchenko.andrew@gmail.com>
2023-12-05 09:54:46 +0200

    modified:   projects/checkov.io/package.yml 	modified:   projects/checkov.io/test.tf

--- projects/checkov.io/test.tf
+++ projects/checkov.io/test.tf
@@ -1,10 +1,30 @@
-resource "aws_s3_bucket_logging" "example" {
-    region        = "us-east-1"
-    bucket        = "aws_s3_bucket.example.id"
-    acl           = "public-read"
-    force_destroy = true
+provider "aws" {
+    region = "eu-west-1"
+}
 
-    versioning {
-        enabled = true
+provider "aws" {
+    alias = "central"
+    region = "eu-central-1"
+}
+
+data "aws_iam_policy_document" "foo_policy" {
+    statement {
+        effect = "Allow"
+        principals = {
+            type = "Service"
+            identifiers = ["ec2.amazonaws.com"]
+        }
+        actions = [
+            "s3:*"
+        ]
+        resources = [
+            "${aws_s3_bucket.foo.arn}"
+        ]
     }
+}
+
+resource "aws_s3_bucket_policy" "bar" {
+    provider = "aws.central"
+    bucket = "${aws_s3_bucket.foo.bucket}"
+    policy = "${data.aws_iam_policy_document.foo_policy.json}"
 }
