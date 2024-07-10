ec391de2b008d476e735692574689e1d11a110dd
Jacob Heider <jacob@pkgx.dev>
2024-07-10 14:07:56 -0400

    fix(llama.cpp)
    llama.cpp deprecated in favor of llama-llama.cpp in v3368
    
    closes #6697

--- projects/github.com/ggerganov/llama.cpp/entrypoint.sh
+++ projects/github.com/ggerganov/llama.cpp/entrypoint.sh
@@ -5,7 +5,7 @@ test -n "$VERBOSE" && set -x
 
 MODEL_URL="https://huggingface.co/TheBloke/dolphin-2.1-mistral-7B-GGUF/resolve/main/dolphin-2.1-mistral-7b.Q4_K_M.gguf"
 MODEL_FILENAME=$(basename "$MODEL_URL")
-MODEL_DIR="${XDG_DATA_HOME:-$HOME/.local/share}"/llama.cpp
+MODEL_DIR="${XDG_DATA_HOME:-$HOME/.local/share}"/llama-cli
 
 if [ ! -f "$MODEL_DIR/$MODEL_FILENAME" ]; then
   gum format <<EoMD
@@ -23,11 +23,11 @@ fi
 D="$(cd "$(dirname "$0")" && pwd)"
 
 if [ $# -gt 0 ]; then
-  exec "$D"/bin/llama.cpp \
+  exec "$D"/bin/llama-cli \
     --model "$MODEL_DIR/$MODEL_FILENAME" \
     "$@"
 else
-  exec "$D"/bin/llama.cpp \
+  exec "$D"/bin/llama-cli \
       --model "$MODEL_DIR/$MODEL_FILENAME" \
       -n 256 \
       --repeat_penalty 1.0 \
