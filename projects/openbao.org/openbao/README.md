3964900b9fc2e7146cf804ebbe7a992f6a954c9a
tannevaled <github@nextonline.org>
2024-08-29 16:37:44 +0200

    add doc and setup

---
+++ projects/openbao.org/openbao/README.md
@@ -0,0 +1,12 @@
+setup
+=====
+
+```bash
+# pkgx +openbao setup
+Package directory /root/.pkgx/openbao.org/openbao/v2.0.0
+install: creating directory '/etc/openbao'
+'/root/.pkgx/openbao.org/openbao/v2.0.0/etc/openbao/openbao.env' -> '/etc/openbao/openbao.env'
+'/root/.pkgx/openbao.org/openbao/v2.0.0/etc/openbao/openbao.hcl' -> '/etc/openbao/openbao.hcl'
+'/root/.pkgx/openbao.org/openbao/v2.0.0/etc/systemd/system/openbao.service' -> '/etc/systemd/system/openbao.service'
+```
+
--- projects/openbao.org/openbao/setup.bash
+++ projects/openbao.org/openbao/setup.bash
@@ -1,3 +1,17 @@
-#!/usr/bin/env -S pkgx +bash^5 bash
+#!/usr/bin/env -S pkgx +gnu.org/coreutils +bash^5 bash
 
-echo 'TO BE DONE'
+function main {
+  declare -r script_file_path="$( realpath "${BASH_SOURCE[0]}" )"
+  declare -r package_bin_path="$( dirname "${script_file_path}" )"
+  declare -r package_path="$( dirname "${package_bin_path}" )"
+  declare -r package_etc_path="${package_path}/etc"
+  declare -r system_etc_path='/etc'
+
+  printf 'Package directory %s\n' ${package_path}
+
+  install -v -D ${package_etc_path}/openbao/openbao.env ${system_etc_path}/openbao/openbao.env
+  install -v -D ${package_etc_path}/openbao/openbao.hcl ${system_etc_path}/openbao/openbao.hcl
+  install -v -D ${package_etc_path}/systemd/system/openbao.service ${system_etc_path}/systemd/system/openbao.service
+}
+
+main
