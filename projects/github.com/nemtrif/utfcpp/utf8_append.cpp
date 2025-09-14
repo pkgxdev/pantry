9ad0db12b98a34489de01ea49f040d5deb1122e1
Jacob Heider <jacob@pkgx.dev>
2025-09-13 23:21:16 -0400

    fix(utf8cpp)
     closes #10479

--- projects/github.com/nemtrif/utfcpp/utf8_append.cpp
+++
@@ -1,6 +0,0 @@
-#include <utf8.h>
-int main() {
-unsigned char u[5] = {0, 0, 0, 0, 0};
-utf8::append(0x0448, u);
-return (u[0] == 0xd1 && u[1] == 0x88 && u[2] == 0 && u[3] == 0 && u[4] == 0) ? 0 : 1;
-}
